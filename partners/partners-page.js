66daeabd84f7d2df63b182d219fb371064da1aa1
Nikolay Stratiev <n.stratiev@gmail.com>
2026-07-14 22:35:30 +0300

    Version 1.0.0

---
+++ partners/partners-page.js
@@ -0,0 +1,54 @@
+// Data
+const videosData = [
+  {
+    title: 'Долината на лимеца',
+    date: '',
+    url: 'https://www.einkornvalley.com/'
+  },
+  {
+    title: 'Евамел',
+    date: '',
+    url: 'https://evamel.com/'
+  },
+  {
+    title: 'Go Guide',
+    date: '',
+    url: 'https://goguide.bg/53212-city-guide-dve-lokatsii-izvun-tsentara-zaradi-koito-si-struva-da-hvanesh-tramvaya/'
+  },
+];
+
+
+// HTML Templates
+const videoTemplate = (data) => html`
+    <div id="video-page">
+        <h2>Партньори</h2>
+        ${data.length === 0 ? noItemsTemplate() : data.map(el => cardTemplate(el))}
+    </div>
+`;
+
+const cardTemplate = (data) => html`
+    <article>
+        <h4><a href="${data.url}" target="_blank">${data.title}</a></h4>
+    </article>
+`;
+
+const noItemsTemplate = () => html`
+    <h3 class="title no-posts-title">Страницата все още няма налично съдържание ...</h3>
+`;
+
+// Refs
+const root = document.querySelector('.page-main');
+
+// Execution
+showVideoPage(root);
+
+// Functions
+function showVideoPage(ctxInput) {
+  render(html`<p class="loader">... Данните се зареждат ...</p>`, ctxInput);
+
+  render(videoTemplate(videosData), ctxInput);
+}
+
+
+// IMPORTS
+import { html, render } from '../GLOBAL/js-global/lib.js';
